affefa6548ac5583c8159ff933a48709e8a47e75
Emmanuel <emma.romero@squad.com.ar>
2022-02-17 15:25:52 -0300

    Init

---
+++ src/main/java/ar/com/eventrust/listaazul/partytimejavaback/EventrustApplication.java
@@ -0,0 +1,13 @@
+package ar.com.eventrust.listaazul.partytimejavaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventrustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventrustApplication.class, args);
+	}
+
+}
